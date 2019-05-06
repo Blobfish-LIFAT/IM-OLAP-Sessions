145e4f70efe90c27a055014c157a3e5f1955384c
ben <bencrulis@gmail.com>
2019-05-06 09:45:26 +0200

    response to comment

--- src/main/java/fr/univ_tours/info/im_olap/Proto1.java
+++ src/main/java/fr/univ_tours/info/im_olap/Proto1.java
@@ -2,6 +2,7 @@ package fr.univ_tours.info.im_olap;
 
 public class Proto1 {
     // I know you don't like static variables but it's easier for now since type are not set yet
+    // just use function arguments
 
 
     public static void main(String[] args) {
